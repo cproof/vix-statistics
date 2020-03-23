df3cfc1f6a603c0705caebf04ca1fa0909778edf
Thomas Schreiber <thomas.schreiber@rtr.at>
2020-03-23 14:14:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is in no way affiliated with the Vienna Internet eXchange, ACOnet or the University of Vienna.
+
+Please visit https://www.vix.at/vix_statistics.html
---
+++ update-graphs.js
@@ -0,0 +1,31 @@
+const https = require('https');
+const fs = require('fs');
+const date = new Date();
+
+const urls = {
+    "daily": "https://www.vix.at/typo3conf/ext/vix_public/Classes/Resource/Traffic/Daily.php",
+    "weekly": "https://www.vix.at/typo3conf/ext/vix_public/Classes/Resource/Traffic/Weekly.php",
+    "monthly": "https://www.vix.at/typo3conf/ext/vix_public/Classes/Resource/Traffic/Monthly.php",
+    "yearly": "https://www.vix.at/typo3conf/ext/vix_public/Classes/Resource/Traffic/Yearly.php"
+};
+
+const target = './images/';
+
+//let filenames contain dates
+const dateStr = date.toISOString().substring(0, 10);;
+
+//download images from the links above
+Object.keys(urls).forEach((key) => {
+    const url = urls[key];
+    const dest = target + dateStr + "_" + key + ".png";
+    const file = fs.createWriteStream(dest);
+    https.get(url, function (response) {
+        response.pipe(file);
+        file.on('finish', function () {
+            file.close();  // close() is async, call cb after close completes.
+        });
+    }).on('error', function (err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    });
+});
+
